3d63810b598ef2a7795ac8f9ba09bfdc96a77c32
Nikitha Kandru <nikitha.kandru@stonybrook.edu>
2017-10-20 18:25:33 -0400

    temporary

--- bcr/WorkloadObj.py
+++ bcr/WorkloadObj.py
@@ -2,9 +2,9 @@ import sys
 
 class WorkloadObj:
     def __init__(self,object):
+        self.action = object[0].strip()
+        self.key = object[1].strip()
         if(len(object)==3):
             self.value = object[2].strip()
         else:
             self.value = None
-        self.action = object[0].strip()
-        self.key = object[1].strip()
