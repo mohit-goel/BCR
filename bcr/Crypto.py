05a60654ecd4b54dfc25d58edc855005008f5316
Mohit <mogoel@cs.stonybrook.edu>
2017-10-21 17:46:48 -0400

    Signing the  orderstatements and resultstatements. Adding crytographic hash support

--- bcr/Crypto.py
+++ bcr/Crypto.py
@@ -2,64 +2,52 @@ import nacl.encoding
 import nacl.signing
 import nacl.exceptions
 import pickle
+import nacl.hash
+from nacl.bindings.utils import sodium_memcmp
 class Crypto:
+    
+    def __init__(self):
+        self.HASHER = nacl.hash.sha256
+   
+    
     
     
     def getSignedKey(self):
         signing_key = nacl.signing.SigningKey.generate()
         verify_key = signing_key.verify_key
-        verify_key_hex = verify_key.encode(encoder=nacl.encoding.HexEncoder)
-        sign_key_hex = signing_key.encode(encoder=nacl.encoding.HexEncoder)
-        return (sign_key_hex,verify_key_hex)
+        #verify_key_hex = verify_key.encode(encoder=nacl.encoding.HexEncoder)
+        #sign_key_hex = signing_key.encode(encoder=nacl.encoding.HexEncoder)
+        return (signing_key,verify_key)
     
-    def sign(self,privateHex,content):
-        signingKey = nacl.signing.SigningKey(privateHex, encoder=nacl.encoding.HexEncoder)
-        return signingKey.sign(content)
+    def sign(self,privateKey,content):
+        #signingKey = nacl.signing.SigningKey(privateHex, encoder=nacl.encoding.HexEncoder)
+        serializedContent = pickle.dumps(content)
+        return privateKey.sign(serializedContent)
         
+    def eq_chk(self,dgs0,dgs1):
+        if sodium_memcmp(dgs0, dgs1):
+            return True
+        return False
     
-    def isSignatureVerified(self,verify_key_hex,signed):
-        verify_key = nacl.signing.VerifyKey(verify_key_hex, encoder=nacl.encoding.HexEncoder)
+    def isSignatureVerified(self,verify_key,signedStatement):
+        #verify_key = nacl.signing.VerifyKey(verify_key_hex, encoder=nacl.encoding.HexEncoder)
         
         # Check the validity of a message's signature
         # Will raise nacl.exceptions.BadSignatureError if the signature check fails
+        message = None
         try:
-            verify_key.verify(signed)
+            message = verify_key.verify(signedStatement)
+            return pickle.loads(message)
         except:
-            return False
+            return message
         
-        return True
+    def getHash(self,content):
+        serializedContent = pickle.dumps(content)
+        digest = self.HASHER(serializedContent, encoder=nacl.encoding.HexEncoder)
+        return digest
     
-    if __name__ == "__main__":
-        signing_key = nacl.signing.SigningKey.generate()
-        orderSt = ",".join([str(1),'put',str(3)])
-        print(orderSt)
+    def verifyHashes(self,content,digest):
+        contentDigest = self.getHash(content)
+        return self.eq_chk(contentDigest,digest)
         
-        #signing_key2 = nacl.signing.SigningKey.generate()
-
-# Sign a message with the signing key
-        signedOrderStatement = signing_key.sign(orderSt.encode())
-        
-        serializedOrderProof = pickle.dumps(signedOrderStatement)
-        
-        orderProof = pickle.loads(serializedOrderProof)
-        
-
-# Obtain the verify key for a given signing key
-        verify_key = signing_key.verify_key
-
-# Serialize the verify key to send it to a third party
-        verify_key_hex = verify_key.encode(encoder=nacl.encoding.HexEncoder)
-
-        #orderSt = slot,operationName,operationId
-        #hexencoder = nacl.encoding.HexEncoder()
-        verify_key = nacl.signing.VerifyKey(verify_key_hex, encoder=nacl.encoding.HexEncoder)
-
-# Check the validity of a message's signature
-# Will raise nacl.exceptions.BadSignatureError if the signature check fails
-        print('hbefdhuvbh')
-        verify_key.verify(orderProof)
-        print('hbefdhuhvgrbvhhhhvbh')
-            
-    
-    
         
