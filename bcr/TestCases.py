98b6620f2b4693ed2bdc46e553c0fa6eead19f4b
Mohit <mogoel@cs.stonybrook.edu>
2017-10-22 14:49:10 -0400

    Adding TestCases class

---
+++ bcr/TestCases.py
@@ -0,0 +1,16 @@
+class TestCases:
+    
+    def __init__(self,testCaseName,masterDict):
+        self.clientToResultDictionary = dict()
+        self.testCaseName =testCaseName
+        self.masterDict = masterDict
+        
+    def verify(self,clientNumber,clientResultDict):
+        print('in verify')
+        return True
+
+
+        
+        
+    
+            
