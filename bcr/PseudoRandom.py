dc76cf43f8d4c10577dd0761a1435809385ae7b6
Mohit <mogoel@cs.stonybrook.edu>
2017-10-22 20:54:17 -0400

    removing undesired imports

--- bcr/PseudoRandom.py
+++ bcr/PseudoRandom.py
@@ -1,13 +1,7 @@
-import numpy as np
-from itertools import permutations
 import random
 class PseudoRandom:
     
-    def select_random_indices(self,num_required_elements, total_indices,seed):
-        np.random.seed(seed)
-        perm = np.random.permutation(total_indices)
-        return perm[0:num_required_elements]
-
+   
     def select_random_elements_of_list(self,l, num_required_elements,seed):
         len_of_list = len(l)            
         sample = []
@@ -17,25 +11,3 @@ class PseudoRandom:
             sample.append(l[index])
        
         return sample
-
-
-if __name__ == "__main__":
-        list2 = ['put','get','slice','append']
-        
-       
-        
-        
-        
-        pseudoRandom  = PseudoRandom()
-        seed = 89
-        lf = pseudoRandom.select_random_elements_of_list(list2,100,seed)
-        
-        #lf = permutations(lf)
-        print(len(lf))
-        for i in lf:
-            print(i)
-        
-        print(pseudoRandom.select_random_elements_of_list(list2,3,seed))
-        
-        
-
