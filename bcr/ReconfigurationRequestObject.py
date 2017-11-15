33ccbbebed6cab863bbb14c9d15b8ed289d443ba
Mohit <mogoel@cs.stonybrook.edu>
2017-11-15 17:43:49 -0500

    Add missing class

---
+++ bcr/ReconfigurationRequestObject.py
@@ -0,0 +1,8 @@
+class ReconfigurationRequestObject:
+    
+    def __init__(self, process):
+        self.process = process
+        
+    def __str__(self):
+        return '['  + str(self.process) + ']'
+        
